85d8a0da19648ac9918ae05995a69cb9d9cf8c1e
leopoldavezac <leopoldavezacgmail.com>
2021-07-29 13:51:04 +0200

    feat(portabilty) - initial v

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='DeepAttribution',
+      version='1.0',
+      description='Attribution Framework',
+      author='Leopold Davezac',
+      author_email='leopoldavezac@gmail.com',
+      url='https://github.com/leopoldavezac/DeepAttribution',
+      packages=['deep_attribution.feature_engineering', 'deep_attribution.model'],
+     )
