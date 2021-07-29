cedfed6538ad4164bdb36f6103d1a40b7ad8a4d0
leopoldavezac <leopoldavezacgmail.com>
2021-07-29 16:11:02 +0200

    refactor(train) - wrap exec in function

--- deep_attribution/train/hosted_training_exec.py
+++ deep_attribution/train/hosted_training_exec.py
@@ -5,66 +5,71 @@ from sagemaker.tensorflow import TensorFlow
 import sagemaker
 
 
-train_instance_type = 'ml.c5.xlarge'
+TRAIN_INSTANCE_TYPE = 'ml.t3.medium'
 
-hyperparameters = {
-        "n_hidden_units_embedding":40,
-        "n_hidden_units_lstm":100,
-        "dropout_lstm":0.2,
-        "recurrent_dropout_lstm":0.1,
-        "learning_rate":0.01,
-        "epochs":5
+
+def run() -> None:
+        
+    hyperparameters = {
+            "n_hidden_units_embedding":40,
+            "n_hidden_units_lstm":100,
+            "dropout_lstm":0.2,
+            "recurrent_dropout_lstm":0.1,
+            "learning_rate":0.01,
+            "epochs":5
+        }
+
+    estimator = TensorFlow(
+        source_dir='deep_attribution/train',
+        entry_point='train.py',
+        model_dir="model",
+        instance_type=TRAIN_INSTANCE_TYPE,
+        instance_count=1,
+        hyperparameters=hyperparameters,
+        role=sagemaker.get_execution_role(),
+        base_job_name='deep-attribution-training',
+        framework_version='2.2',
+        py_version='py37',
+        script_mode=True
+        )
+
+
+    hyperparameter_ranges = {
+        'learning_rate': ContinuousParameter(0.001, 0.2, scaling_type="Logarithmic"),
+        'epochs': IntegerParameter(10, 50),
+        'n_hidden_units_embedding': IntegerParameter(20, 100),
+        "n_hidden_units_lstm":IntegerParameter(64, 256),
+        "dropout_lstm":ContinuousParameter(0.001, 0.3, scaling_type="Logarithmic"),
+        "recurrent_dropout_lstm":ContinuousParameter(0.001, 0.3, scaling_type="Logarithmic")
     }
 
-estimator = TensorFlow(
-    source_dir='deep_attribution/train',
-    entry_point='train.py',
-    model_dir="model",
-    instance_type=train_instance_type,
-    instance_count=1,
-    hyperparameters=hyperparameters,
-    role=sagemaker.get_execution_role(),
-    base_job_name='deep-attribution-training',
-    framework_version='2.2',
-    py_version='py37',
-    script_mode=True
-    )
+    metric_definitions = [{
+        'Name': 'test_auc',
+        'Regex': ' auc - test: ([0-9\\.]+)'}
+        ]
+
+    objective_metric_name = 'test_auc'
+    objective_type = 'Maximize'
 
 
-hyperparameter_ranges = {
-    'learning_rate': ContinuousParameter(0.001, 0.2, scaling_type="Logarithmic"),
-    'epochs': IntegerParameter(10, 50),
-    'n_hidden_units_embedding': IntegerParameter(20, 100),
-    "n_hidden_units_lstm":IntegerParameter(64, 256),
-    "dropout_lstm":ContinuousParameter(0.001, 0.3, scaling_type="Logarithmic"),
-    "recurrent_dropout_lstm":ContinuousParameter(0.001, 0.3, scaling_type="Logarithmic")
-}
+    tuner = HyperparameterTuner(estimator,
+                                objective_metric_name,
+                                hyperparameter_ranges,
+                                metric_definitions,
+                                max_jobs=15,
+                                max_parallel_jobs=5,
+                                objective_type=objective_type)
 
-metric_definitions = [{
-    'Name': 'test_auc',
-    'Regex': ' auc - test: ([0-9\\.]+)'}
-    ]
-
-objective_metric_name = 'test_auc'
-objective_type = 'Maximize'
+    tuning_job_name = "deep-attribution-training-{}".format(strftime("%d-%H-%M-%S", gmtime()))
+    tuner.fit(job_name=tuning_job_name)
+    tuner.wait()
 
 
-tuner = HyperparameterTuner(estimator,
-                            objective_metric_name,
-                            hyperparameter_ranges,
-                            metric_definitions,
-                            max_jobs=15,
-                            max_parallel_jobs=5,
-                            objective_type=objective_type)
+    tuning_predictor = tuner.deploy(initial_instance_count=1, instance_type='ml.m5.xlarge')
 
-tuning_job_name = "deep-attribution-training-{}".format(strftime("%d-%H-%M-%S", gmtime()))
-tuner.fit(job_name=tuning_job_name)
-tuner.wait()
+    estimator = tuning_predictor.best_estimator()
+    estimator_path = estimator.model_data
 
+    print(estimator_path)
 
-tuning_predictor = tuner.deploy(initial_instance_count=1, instance_type='ml.m5.xlarge')
-
-estimator = tuning_predictor.best_estimator()
-estimator_path = estimator.model_data
-
-# batch prediction
+    # batch prediction
--- deep_attribution/train/local_train_exec.py
+++ deep_attribution/train/local_train_exec.py
@@ -8,41 +8,43 @@ from deep_attribution.train.utilities import get_X_sample
 # !/bin/bash ./local_mode_setup.sh
 
 
-sess = sagemaker.Session()
+def run() -> None:
+
+    sagemaker.Session()
 
 
-model_dir = 'model'
-train_instance_type = 'local'
-hyperparameters = {
-        "n_hidden_units_embedding":40,
-        "n_hidden_units_lstm":100,
-        "dropout_lstm":0.2,
-        "recurrent_dropout_lstm":0.1,
-        "learning_rate":0.01,
-        "epochs":5
-    }
+    model_dir = 'model'
+    train_instance_type = 'local'
+    hyperparameters = {
+            "n_hidden_units_embedding":40,
+            "n_hidden_units_lstm":100,
+            "dropout_lstm":0.2,
+            "recurrent_dropout_lstm":0.1,
+            "learning_rate":0.01,
+            "epochs":5
+        }
 
-local_estimator = TensorFlow(
-    source_dir='deep_attribution/train',
-    entry_point='train.py',
-    model_dir=model_dir,
-    instance_type=train_instance_type,
-    instance_count=1,
-    hyperparameters=hyperparameters,
-    role=sagemaker.get_execution_role(),
-    base_job_name='deep-attribution-local-training',
-    framework_version='2.2',
-    py_version='py37',
-    script_mode=True
-    )
-
-
-local_estimator.fit()
+    local_estimator = TensorFlow(
+        source_dir='deep_attribution/train',
+        entry_point='train.py',
+        model_dir=model_dir,
+        instance_type=train_instance_type,
+        instance_count=1,
+        hyperparameters=hyperparameters,
+        role=sagemaker.get_execution_role(),
+        base_job_name='deep-attribution-local-training',
+        framework_version='2.2',
+        py_version='py37',
+        script_mode=True
+        )
 
 
-X_sample = get_X_sample()
+    local_estimator.fit()
 
-local_predictor = local_estimator.deploy(initial_instance_count=1, instance_type='local')
-local_results = local_predictor.predict(X_sample)['predictions']
 
-local_predictor.delete_endpoint()
+    X_sample = get_X_sample()
+
+    local_predictor = local_estimator.deploy(initial_instance_count=1, instance_type='local')
+    print(local_predictor.predict(X_sample)['predictions'])
+
+    local_predictor.delete_endpoint()
