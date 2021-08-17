2b34bf01faf4b3f6c1635d6028a1291e9da325a8
leopoldavezac <leopoldavezacgmail.com>
2021-08-17 16:53:29 +0200

    fix(setup) - change package_nms

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='DeepAttribution',
+setup(name='deep_attribution',
       version='1.0',
       description='Attribution Framework',
       author='Leopold Davezac',
       author_email='leopoldavezac@gmail.com',
       url='https://github.com/leopoldavezac/DeepAttribution',
-      packages=['deep_attribution'],
+      packages=[
+            "deep_attribution.feature_engineering",
+            "deep_attribution.preprocess",
+            "deep_attribution.train",
+            "deep_attribution.model"
+            ],
      )
