0363f6723b09f6d8d13655cb0e2d5f0a42f27cf2
leopoldavezac <leopoldavezacgmail.com>
2021-08-22 13:10:05 +0200

    fix(deep_attribution) - instance type and count from config must not be passed to job

--- deep_attribution/utilities.py
+++ deep_attribution/utilities.py
@@ -2,8 +2,11 @@ from typing import Dict, List
 
 def format_config_as_job_args(config: Dict) -> List[str]:
 
+    config_keys_to_exclude = ["feature_engineering", "preprocessing", "training", "attention_report_generation"]
+
     job_args = []
     for arg_nm, arg_val in config.items():
-        job_args += ["--"+arg_nm, arg_val]
+        if arg_nm not in config_keys_to_exclude:
+            job_args += ["--"+arg_nm, arg_val]
 
     return job_args
