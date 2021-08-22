0bac0793f078567f0da6e7257f032bf5852d9f46
leopoldavezac <leopoldavezacgmail.com>
2021-08-22 12:56:35 +0200

    fix(setup) - packages args is deep_attribution only

--- setup.py
+++ setup.py
@@ -6,12 +6,5 @@ setup(name='deep_attribution',
       author='Leopold Davezac',
       author_email='leopoldavezac@gmail.com',
       url='https://github.com/leopoldavezac/DeepAttribution',
-      packages=[
-            "deep_attribution.feature_engineering",
-            "deep_attribution.preprocess",
-            "deep_attribution.train",
-            "deep_attribution.model",
-            "deep_attribution.generate_attention_report",
-            "deep_attribution.utilities"
-            ],
+      packages=["deep_attribution"],
      )
