168f9c175c773905143c5bb63e6d648c04881c0f
leopoldavezac <leopoldavezacgmail.com>
2021-08-24 14:19:33 +0200

    feat(train) - oversampling implementation

--- deep_attribution/train/oversampling.py
+++ deep_attribution/train/oversampling.py
@@ -6,5 +6,20 @@ from numpy.random import choice
 
 
 def oversample(X: ndarray, y:ndarray) -> List[ndarray]:
+    
+    filter_true = y == True
+    filter_false = ~filter_true
 
-    pass
+    X_false = X[filter_false]
+    y_false = y[filter_false]
+
+    indices_true = flatnonzero(y)
+    oversampled_indices = choice(indices_true, size=len(y_false))
+
+    X_true_resampled = X[oversampled_indices]
+    y_true_resampled = y[oversampled_indices]
+
+    X_resampled = concatenate([X_true_resampled, X_false], axis=0)
+    y_resampled = concatenate([y_true_resampled, y_false], axis=0)
+
+    return [X_resampled, y_resampled]
